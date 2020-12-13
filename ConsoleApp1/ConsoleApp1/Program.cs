eb6ca89516dec08c3336994fde9c3e566d408235
SANA BEN <sana.bengannoune@gmail.com>
2020-12-13 18:10:24 +0100

    Demarer program done

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,29 +8,93 @@ namespace ConsoleApp1
         {
             try
             {
+
+
                 Banque bq = new Banque();
-                bq.AjouterCompteSurCarnet(123,1003,"salima",20000);
-                bq.AjouterCompteSurCarnet(150, 2089, "sana", 500);
-                bq.AjouterCompteSurCheque(1002, 56356, "10 octobre 2022", 302, "ilham", 2000);
-                bq.AjouterCompteSurCheque(1003, 87976, "10 octobre 2022", 300, "karim", 2300);
-                bq.AfficherList();
-                Console.WriteLine("---------------fin affichage de list---------------------------");
+                void Demarer()
+                {
+                    Console.WriteLine("Detrminer votre operation : Add, Search, Delete,DetailsCompte, ChekList, Debiter,crediter");
+                    string opt = Console.ReadLine();
+                    Choix(opt);
+                    void Choix( string opt)
+                    {
+                        if (opt == "Add")
+                        {
+                            Console.WriteLine("Enter le type de compte : CompteSurCarnet , CompteSurCheque");
+                            string type = Console.ReadLine();
 
-                bq.ChercherCompte(2089);
-                Console.WriteLine("---------------fin de recuperer l'indice de compte---------------------------");
+                            if(type == "CompteSurCarnet")
+                            {
+                                Console.WriteLine("Enter les info du compte : NumeroCarnet ,NumeroCompte, NomProprietaire , Solde");
+                                int n1 = int.Parse(Console.ReadLine());
+                                int n = int.Parse(Console.ReadLine());
+                                string p = Console.ReadLine();
+                                decimal s = decimal.Parse(Console.ReadLine());
+                                bq.AjouterCompteSurCarnet( n1, n, p, s);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Enter les info du compte : NumeroCheque,NumeroCarte,DateValidationCarte ,NumeroCompte, NomProprietaire , Solde");
+                               
+                                int n1 = int.Parse(Console.ReadLine());
+                                int n2 = int.Parse(Console.ReadLine());
+                                string t = Console.ReadLine();
+                                int n = int.Parse(Console.ReadLine());
+                                string p = Console.ReadLine();
+                                decimal s = decimal.Parse(Console.ReadLine());
+                                bq.AjouterCompteSurCheque( n1,  n2, t, n, p, s);
+                            }
 
-                bq.DeleteCompte(2089);
-                bq.AfficherList();
-                Console.WriteLine("---------------fin verifier notre delet compte---------------------------");
-                bq.ChercherCompte(2089);
-                Console.WriteLine("---------------fin chercher le compte suprimé--------------------------");
-                bq.GetInfoCompte(302);
-                Console.WriteLine("---------------fin afficher info d'un compte donnee---------------------------");
-                bq.OperationDebiter(1003, 10500);
-                Console.WriteLine("---------------fin d'une operation debiter compte--------------------------");
-                bq.OperationCrediter(302, 300);
-                Console.WriteLine("---------------fin d'une operation crediter compte--------------------------");
+                        }
+                        else if (opt == "Search")
+                        {
+                            Console.WriteLine("Enter le numero  de compte :");
+                            int numero = int.Parse(Console.ReadLine());
+                            bq.ChercherCompte(numero);
 
+                        }
+                        else if (opt == "Delete")
+                        {
+                            Console.WriteLine("Enter le numero  de compte :");
+                            int numero = int.Parse(Console.ReadLine());
+                            bq.DeleteCompte(numero);
+
+                        }
+                        else if (opt == "DetailsCompte")
+                        {
+                            Console.WriteLine("Enter le numero  de compte :");
+                            int numero = int.Parse(Console.ReadLine());
+                            bq.GetInfoCompte(numero);
+
+                        }
+                        else if (opt == "ChekList")
+                        {
+                            bq.AfficherList();
+                        }
+                        else if (opt == "Debiter")
+                        {
+                            Console.WriteLine("Enter le numero  de compte, le montant :");
+                            int numero = int.Parse(Console.ReadLine());
+                            decimal montant = decimal.Parse(Console.ReadLine());
+                            bq.OperationDebiter(numero,montant);
+                        }
+                        else if (opt == "Crediter")
+                        {
+                            Console.WriteLine("Enter le numero  de compte , le montant ");
+                            int numero = int.Parse(Console.ReadLine());
+                            decimal montant = decimal.Parse(Console.ReadLine());
+                            bq.OperationCrediter(numero, montant);
+                        }
+                        Demarer();
+                    }
+
+
+
+
+
+
+                }
+                Demarer();
 
 
             }
